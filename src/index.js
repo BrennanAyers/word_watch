000f83bfa03d158df76bb61a587cc3fdb93e3309
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-09-05 10:35:36 -0600

    Add helper methods and fetch call to get Top Word from API for Top Word section on page load

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 import $ from 'jquery'
 
+
+function createNode(element) {
+  return document.createElement(element); // Create the type of element you pass in the parameters
+}
+
+function append(parent, el) {
+  return parent.appendChild(el); // Append the second parameter(element) to the first one
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 $(document).ready(() => {
-  // have fun!
+  const word_count_article = document.getElementById('word-count')
+  const url = 'https://wordwatch-api.herokuapp.com/api/v1/top_word'
+  fetch(url)
+    .then(data => {
+      return data.json()
+        .then(json => {
+          let p = createNode('p')
+          p.innerHTML = `${capitalizeFirstLetter(Object.keys(json.word)[0])}: ${Object.values(json.word)[0]} times`
+          return append(word_count_article, p)
+        })
+      })
+    .catch(error => {
+      console.log(error)
+    })
 })
